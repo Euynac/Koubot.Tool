87724ea0d433b9573a16889fc6c8903f337c65b7
Banyc <36535895+Banyc@users.noreply.github.com>
2021-02-12 18:53:41 +0800

    Add new method for removing escape chars

--- General/EscapeTool.cs
+++ General/EscapeTool.cs
@@ -1,4 +1,6 @@
-﻿namespace Koubot.Tool.General
+﻿using System.Text;
+
+namespace Koubot.Tool.General
 {
     /// <summary>
     /// 转义工具类 用于转义一些在某些场合可能引发问题的字符串
@@ -24,5 +26,50 @@
         }
 
         //'(', ')', '[', ']', '{', '}', '!', '?', '^', '|', ',', ':','.', '+', '*', '=', '$', '#', '<', '>', '@', '%', '&', '-', '_', '\'', '"'
+
+        private enum RemoveEscapeCharsStates
+        {
+            Reset,
+            FoundEscapeChar
+        }
+        /// <summary>
+        /// Remove escape chars from text.
+        /// </summary>
+        /// <param name="originalText"></param>
+        /// <param name="escapeChar"></param>
+        /// <returns></returns>
+        public static string RemoveEscapeChars(string originalText, char escapeChar = '\\')
+        {
+            StringBuilder result = new StringBuilder();
+            RemoveEscapeCharsStates state = RemoveEscapeCharsStates.Reset;
+            foreach (char chr in originalText)
+            {
+                switch (state)
+                {
+                    case RemoveEscapeCharsStates.Reset:
+                        if (chr == escapeChar)
+                        {
+                            state = RemoveEscapeCharsStates.FoundEscapeChar;
+                        }
+                        else
+                        {
+                            result.Append(chr);
+                            state = RemoveEscapeCharsStates.Reset;
+                        }
+                        break;
+                    case RemoveEscapeCharsStates.FoundEscapeChar:
+                        result.Append(chr);
+                        state = RemoveEscapeCharsStates.Reset;
+                        break;
+                    default:
+                        throw new System.Exception("Unknown state");
+                }
+            }
+            if (state != RemoveEscapeCharsStates.Reset)
+            {
+                throw new System.Exception($"{state} is not an accept state!");
+            }
+            return result.ToString();
+        }
     }
 }
